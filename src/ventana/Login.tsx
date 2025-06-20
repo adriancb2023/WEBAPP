32b85dc9574c060e8a1d048bec3ca351792a7fe7
Adrian Cruz Barranco <145365245+adriancb2023@users.noreply.github.com>
2025-06-20 18:44:28 +0200

    a

--- src/ventana/Login.tsx
+++ src/ventana/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
-import Aurora from './Aurora'
-import './App.css'
+import Aurora from '../componentes/Aurora'
+import '../App.css'
 
 const supabaseUrl = 'https://qarctnyssctoosibzqik.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhcmN0bnlzc2N0b29zaWJ6cWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0MjgyNjAsImV4cCI6MjA2NjAwNDI2MH0.8ypxqQY8ONP2hJzYyeaN7L9GK07DGfSekqAU_4ARkOw'
